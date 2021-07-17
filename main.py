8558480b264bfe6ef4883707759f7be46b2785bd
Aniket Patil <55218425+aniketpatil03@users.noreply.github.com>
2021-07-17 15:28:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,16 +4,12 @@ screen = Screen()
 screen.setup(height=600, width=600)
 screen.bgcolor("light cyan")
 
-snake_segment_1 = Turtle(shape="square")
-snake_segment_1.color("Purple")
+snake_positions = [(0, 0), (-20, 0), (-40, 0)]
 
-snake_segment_2 = Turtle(shape="square")
-snake_segment_2.color("Purple")
-snake_segment_2.goto(-20, 0)   # in turtle each pixel is 20px hence from first segment it moves back 20 ie -20
-
-snake_segment_3 = Turtle(shape="square")
-snake_segment_3.color("Purple")
-snake_segment_3.goto(-40, 0)   # in turtle each pixel is 20px hence from first segment it moves back more 20 ie -40
+for position in snake_positions:
+    snake_segment = Turtle(shape="square")
+    snake_segment.color("Purple")
+    snake_segment.goto(position)
 
 
 
