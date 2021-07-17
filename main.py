364fe8991919ded437f28d1a86a95f6a9c73aa9d
Aniket Patil <55218425+aniketpatil03@users.noreply.github.com>
2021-07-17 15:58:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,12 +6,19 @@ screen.bgcolor("light cyan")
 
 snake_positions = [(0, 0), (-20, 0), (-40, 0)]
 
+segment_list = []
+
 for position in snake_positions:
     snake_segment = Turtle(shape="square")
     snake_segment.color("Purple")
+    snake_segment.penup()
     snake_segment.goto(position)
+    segment_list.append(snake_segment)
 
-
+game_on = True
+while game_on:
+    for seg in segment_list:
+        seg.forward(40)
 
 
 screen.exitonclick()
