bc8afe1fa72a925a2a2bc8a63e32d6ab6be4ccee
Aniket Patil <55218425+aniketpatil03@users.noreply.github.com>
2021-07-25 18:37:51 +0530

    Create snake_updated.py

---
+++ snake_updated.py
@@ -0,0 +1,52 @@
+from turtle import Turtle
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
+
+class Snake:
+
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+        self.head = self.segments[0]
+
+    def create_snake(self):
+        # logic is 3 moves in position of 2 and 2 in position of 1 thus this way all are connected
+        # or else 1 will get detached from 2 and three when it changes its direction
+
+        # taking from reverse order as 3 replaces two first and len()- 1 as index starts from zero
+        for position in STARTING_POSITIONS:
+            new_segment = Turtle("square")
+            new_segment.color("white")
+            new_segment.penup()
+            new_segment.goto(position)
+            self.segments.append(new_segment)
+
+    def move(self):
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[seg_num - 1].xcor()
+                                                    # here segment_list[seg_num-1] gives second last's x cor
+            new_y = self.segments[seg_num - 1].ycor()
+                                                    # here segment_list[seg_num-1] gives second last's y cor
+            self.segments[seg_num].goto(new_x, new_y)
+        self.head.forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
