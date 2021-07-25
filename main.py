a8fcb912a83ae4f5736a0a8d40d056a463833343
Aniket Patil <55218425+aniketpatil03@users.noreply.github.com>
2021-07-25 18:35:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,27 @@
+from turtle import Screen
+from snake import Snake
 import time
-from turtle import Screen, Turtle
-from time import sleep
 
 screen = Screen()
-screen.setup(height=600, width=600)
-screen.bgcolor("light cyan")
+screen.setup(width=600, height=600)             # setting the screen window
+screen.bgcolor("black")
+screen.title("My Snake Game")
+screen.tracer(0)         # makes all animations disappear
 
-snake_positions = [(0, 0), (-20, 0), (-40, 0)]
-screen.tracer(0)         # Turns turtle animation on/off and set delay for update drawings.
-segment_list = []
+snake = Snake()
 
-for position in snake_positions:
-    snake_segment = Turtle(shape="square")
-    snake_segment.color("Purple")
-    snake_segment.penup()
-    snake_segment.goto(position)
-    segment_list.append(snake_segment)
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
 
-game_on = True
-while game_on:
-    screen.update()    # updates the screen
+game_is_on = True
+while game_is_on:
+    screen.update()           # Animation display appears back here
     time.sleep(0.1)
 
-# logic is 3 moves in position of 2 and in position of 1 thus this way all are connected
-# or else 1 will get deatched from 2 and three when it changes its direction
-    for seg_num in range(len(segment_list)-1, 0, -1):   # taking from reverse order as 3 replaces two first and len()- 1 as index starts from zero
-        new_x = segment_list[seg_num - 1].xcor()
-        new_y = segment_list[seg_num - 1].ycor()
-        segment_list[seg_num].goto(new_x, new_y)
-    segment_list[0].forward(20)
-    segment_list[0].left(90)
+    snake.move()
+
+
 screen.exitonclick()
